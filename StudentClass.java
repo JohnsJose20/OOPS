0d5a75fd8babef8a430f480ddcbde92e783b6fb3
Johns Jose <jjkanichukattu@gmail.com>
2025-07-23 09:55:24 +0530

    Add files via upload

---
+++ StudentClass.java
@@ -0,0 +1,21 @@
+
+class Student {
+	String name;
+	int age;
+
+void displayDetails() {
+	System.out.println("Name: "+name);
+	System.out.println("Age: "+age);
+}
+}
+public class StudentClass {
+	public static void main(String[]args) {
+		Student student1 = new
+		
+Student();
+			student1.name="Johns Jose";
+			student1.age=18;
+		
+			student1.displayDetails();
+	}
+}
