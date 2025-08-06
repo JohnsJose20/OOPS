bd8e753b30a70c8406ec74d3aa28bd598f0442e3
Johns Jose <jjkanichukattu@gmail.com>
2025-08-06 10:49:37 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,17 @@
+package pack1;
+class Book{
+	String title;
+	String authore;
+	final int bookId;
+	static int bookCounter=1000;
+	static final String LIBARY_NAME = "Central Library";
+	
+	Book (){
+		
+	}
+	
+	Book (String title,String authore){
+		this.bookId=bookCounter++;
+		this.
+	}
+	
