ecfc456cbd5185401c2e620c4b6c622df88388db
Johns Jose <jjkanichukattu@gmail.com>
2025-08-13 10:42:21 +0530

    Add files via upload

---
+++ StringArray.java
@@ -0,0 +1,14 @@
+package package1;
+
+public class StringArray {
+    public static void main(String[] args) {
+        
+        String[]  color = {"Red","Orange","White","Black"};
+
+        System.out.println("String Array:");
+        for (int i = 0; i < color.length; i++) {
+        	 System.out.print(color[i]+", ");
+        }
+
+}
+}
