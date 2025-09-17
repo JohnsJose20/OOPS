d0a8800d9751f25ca995c8ff907420aad008f3ad
Johns Jose <jjkanichukattu@gmail.com>
2025-09-17 10:41:07 +0530

    Add files via upload

--- ExceptionDemo.java
+++ ExceptionDemo.java
@@ -3,7 +3,14 @@ package package1;
 public class ExceptionDemo {
 	public static void main(String[]args) {
 		
+		try{
+			
+		//NumberFormatException
+		String s2="abc";
+		System.out.println(Integer.parseInt(s2));
+			
 		//IndexOutOfBoundException(Integer)
+		
 		int [] array = {1,2,3,4,5};
 		System.out.println(array[6]);
 		
@@ -11,16 +18,21 @@ public class ExceptionDemo {
 		String college = "SJCET";
 		System.out.println(college.charAt(10));
 		
-		//NumberFormatException
-		String s2="abc";
-		System.out.println(Integer.parseInt(s2));
-		
 		//NullPointerException
 		String s1=null;
 		System.out.println(s1.length());
 		
 		//ArithemeticException
 		System.out.println(10/0);
+		System.out.println("After Exception");
+		
+		} catch (ArithmeticException ae) {
+			
+		}
+		 catch (NumberFormatException ne) {
+			 System.out.println("No format Exception");
+		}
+		
 		System.out.println("After Exception");
 	}
 }
